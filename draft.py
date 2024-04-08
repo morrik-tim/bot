24c7156d2eb7c046795b77be78627ddc6f98db89
morrik <marattim83@gmail.com>
2024-04-08 22:30:41 +0500

    тест

--- draft.py
+++ draft.py
@@ -10,3 +10,20 @@
 # video = stream.video
 # print(await video.last_url)
 # print((await video[video.min].last_url).mp4, end='\n\n')  # worst quality (.mp4)
+
+# async with aiohttp.ClientSession() as session:
+#     async with session.get(video_url) as response:
+#         if response.status == 200:
+#             content_length = int(response.headers.get('Content-Length', 0))
+#             with tqdm(total=content_length, unit='B', unit_scale=True, desc=video_url.split('/')[-1]) as pbar:
+#                 async with aiofiles.open(video_url.split('/')[-1], mode='wb') as f:
+#                     asyncio.StreamWriter(response.content, f)
+#                     pbar.update(content_length)
+#                 await telethon_client.send_file(
+#                     chat_id, video_url.split('/')[-1],
+#                     supports_streaming=True,
+#                     attributes=[DocumentAttributeVideo(seconds, width_clip, height_clip, supports_streaming=True)]
+#                 )
+#             logging.info("Видео отправлено!")
+#         else:
+#             logging.error(f"Failed to download video: {response.status}")
--- prime.py
+++ prime.py
@@ -12,6 +12,8 @@ from hdrezka import Search
 from moviepy.editor import VideoFileClip
 from telethon import TelegramClient
 from telethon.tl.types import DocumentAttributeVideo
+from tqdm import tqdm
+from asyncio import Queue
 
 load_dotenv(find_dotenv())
 
@@ -27,25 +29,39 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
+video_g = None
+video_queue = asyncio.Queue()
+
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
+    print('начало работ')
+    await message.answer(f'ID чата {message.chat.id}')
     await message.answer("Введите название фильма или сериала.")
-    print(message.chat.id)
-    await message.answer(message.chat.id)
+    logging.info(f"Chat ID: {message.chat.id}")
 
 
 @dp.message_handler(content_types=['text'])
 async def main(message: types.Message):
-    async with aiohttp.ClientSession() as session:
-        await process_search_results(message.text, session, message.chat.id)
+    async with aiohttp.ClientSession():
+        try:
+            markup = await get_markup()
+            await process_search_results(message.text, message.chat.id, markup)
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
 
 
-async def process_search_results(query, session, chat_id):
-    markup = await get_markup()
+@dp.callback_query_handler(lambda query: query.data == 'select')
+async def select_callback_handler(query: types.CallbackQuery):
+    await bot.send_message(query.message.chat.id, "принято в работу")
+    await process_video(query.message.chat.id)
+
+
+async def process_search_results(query, chat_id, markup):
+    print('работаем')
     page = 1
     async for film in search_films(query, page):
-        await process_film(film, markup, session, chat_id)
+        await process_film(film, markup, chat_id)
 
 
 async def search_films(query, start_page):
@@ -54,36 +70,41 @@ async def search_films(query, start_page):
         results = len(search_results)
         if results == 0:
             logging.info("No more search results found.")
-            break  # Выход из цикла, если результатов нет
+            break
         for film in range(results):
             yield search_results[film]
         if results < 36:
             logging.info("Reached end of search results.")
-            break  # Выход из цикла, если результатов меньше 36
+            break
         start_page += 1
 
 
-async def process_film(film, markup, session, chat_id):
+async def process_film(film, markup, chat_id):
+    global video_g
     player = await film.player
     meta_tag = player.post._soup_inst.find('meta', property='og:type')
     content = meta_tag['content'].removeprefix('video.')
     logging.info(f'Название - {player.post.name}')
     logging.info(f'Тип контента - {content}')
-    await bot.send_photo(chat_id, film.poster, player.post.name, reply_markup=markup)
+    try:
+        await bot.send_photo(chat_id, film.poster, film.name, reply_markup=markup)
+    except Exception as e:
+        logging.error(f"An error occurred while sending photo: {e}")
 
     await asyncio.sleep(2)
 
     translator_id = next((id_ for name, id_ in player.post.translators.name_id.items() if 'Дубляж' in name), None)
     if translator_id:
         stream = await player.get_stream(translator_id)
-        video = stream.video
+        video_g = stream.video
+        await video_queue.put(video_g)
 
-        await process_video(video, chat_id)
 
-
-async def process_video(video, chat_id):
-    for i, quality in enumerate(video.qualities):
-        if quality == 720:
+async def process_video(chat_id):
+    print('процесс')
+    video = await video_queue.get()
+    for i in range(len(video.qualities)):
+        if video.qualities[i] == 360:
             video_url = (await video[i].last_url).mp4
             seconds, width_clip, height_clip = await get_video_params(video_url)
             await send_video(video_url, seconds, width_clip, height_clip, chat_id)
@@ -111,18 +132,25 @@ async def send_video(video_url, seconds, width_clip, height_clip, chat_id):
     async with aiohttp.ClientSession() as session:
         async with session.get(video_url) as response:
             if response.status == 200:
-                async with aiofiles.open(video_url.split('/')[-1], mode='wb') as f:
-                    while True:
-                        chunk = await response.content.read(8192)
-                        if not chunk:
-                            break
-                        await f.write(chunk)
-            await telethon_client.send_file(
-                chat_id, video_url.split('/')[-1],
-                supports_streaming=True,
-                attributes=[DocumentAttributeVideo(seconds, width_clip, height_clip, supports_streaming=True)]
-            )
-            logging.info("Видео отправлено!")
+                content_length = int(response.headers.get('Content-Length', 0))
+                with tqdm(total=content_length, unit='B', unit_scale=True, desc=video_url.split('/')[-1]) as pbar:
+                    async with aiofiles.open(video_url.split('/')[-1], mode='wb') as f:
+                        while True:
+                            chunk = await response.content.read(1048576)
+                            if not chunk:
+                                break
+                            await f.write(chunk)
+                            pbar.update(len(chunk))
+                            print(f'прогресс - {pbar.update(len(chunk))}')
+                            print(f'прогресс - {pbar}')
+                    await telethon_client.send_file(
+                        chat_id, video_url.split('/')[-1],
+                        supports_streaming=True,
+                        attributes=[DocumentAttributeVideo(seconds, width_clip, height_clip, supports_streaming=True)]
+                    )
+                    logging.info("Видео отправлено!")
+            else:
+                logging.error(f"Failed to download video: {response.status}")
 
 
 if __name__ == '__main__':
