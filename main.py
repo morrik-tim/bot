1b1afb57dff763b97bbcb1fd262ad6a7376fab66
morrik <marattim83@gmail.com>
2024-04-14 14:31:18 +0500

    по настоящему первый рабочий прототип

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from aiogram.utils import executor
 from dotenv import load_dotenv, find_dotenv
 from hdrezka import Search
 from moviepy.editor import VideoFileClip
+from telethon import TelegramClient
+from telethon.tl.types import DocumentAttributeVideo
 from tqdm import tqdm
 
 load_dotenv(find_dotenv())
@@ -20,17 +22,22 @@ PHONE = os.getenv("PHONE")
 TOKEN = os.getenv("TOKEN")
 
 logging.basicConfig(level=logging.INFO)
-# telethon_client = TelegramClient('anon', API_ID, API_HASH)
-# telethon_client.start(phone=PHONE)
+telethon_client = TelegramClient('anon', API_ID, API_HASH)
+telethon_client.start(phone=PHONE)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
-page_queue = asyncio.Queue()
-film_queue = asyncio.Queue()
-search_results_queue = asyncio.Queue()
-markup_queue = asyncio.Queue()
+player = None
+search_results = None
+film = None
+page = None
+markup_main = None
+choose_markup = None
+choose_quality = None
+video = None
+translator_id = None
 
 
 @dp.message_handler(commands=['start'])
@@ -40,19 +47,20 @@ async def start(message: types.Message):
     logging.info(f"Chat ID: {message.chat.id}")
 
 
+@dp.message_handler(commands=['clear'])
+async def clear(message: types.Message):
+    await message.delete()
+
+
 @dp.message_handler(content_types=['text'])
 async def main(message: types.Message):
-    global player
+    global player, search_results, film, markup_main, choose_markup, page
 
-    await markup_queue.put(await markups())
-    await page_queue.put(1)
-    await film_queue.put(0)
+    film = 0
+    page = 1
+    markup_main = await main_markups()
 
-    film = await film_queue.get()
-    markup = await markups()
-
-    search_results = await Search(message.text).get_page(page_queue)
-    await search_results_queue.put(search_results)
+    search_results = await Search(message.text).get_page(page)
 
     player = await search_results[film].player
 
@@ -62,29 +70,58 @@ async def main(message: types.Message):
     print(f'Название - {player.post.name}')
     print(f'Тип контента - {content}')
 
-    await message.answer_photo(search_results[film].poster, player.post.name, reply_markup=markup)
+    film = 0
 
-    await film_queue.put(film)
-    await markup_queue.put(markup)
+    await message.answer_photo(search_results[film].poster, player.post.name, reply_markup=markup_main)
+    choose_markup = await choose_translator_markups()
 
 
 @dp.callback_query_handler(lambda query: query.data == 'select')
 async def select_callback_handler(query: types.CallbackQuery):
-    print('Выбрано')
+    await query.message.delete()
+    await bot.send_message(query.message.chat.id, 'Выберите озвучку', reply_markup=choose_markup)
 
 
 @dp.callback_query_handler(lambda query: query.data == 'next')
 async def next_callback_handler(query: types.CallbackQuery):
+    await query.message.delete()
     await next_film(query.message.chat.id)
 
 
 @dp.callback_query_handler(lambda query: query.data == 'back')
 async def back_callback_handler(query: types.CallbackQuery):
+    await query.message.delete()
     await back_film(query.message.chat.id)
 
 
+@dp.callback_query_handler(lambda query: query.data in player.post.translators.name_id.keys())
+async def translator_callback_handler(query: types.CallbackQuery):
+    global translator_id
+    translator_name = query.data
+    translator_id = player.post.translators.name_id[translator_name]  # id'shnik
+    # Далее вы можете выполнить какие-то действия в зависимости от выбранного переводчика
+    await  process_film()
+    choose_quality = await choose_quality_markups()
+    await query.message.delete()
+    await bot.send_message(query.message.chat.id, f'Выберете качество', reply_markup=choose_quality)
+
+
+@dp.callback_query_handler(lambda query: query.data.isdigit() and int(query.data) < len(video.qualities))
+async def choose_quality_callback_handler(query: types.CallbackQuery):
+    await query.message.delete()
+    chosen_quality_index = int(query.data)
+    chosen_quality = video.qualities[chosen_quality_index]
+
+    # Действия, которые нужно выполнить при выборе качества видео
+    # Например, отправка сообщения с выбранным качеством
+    await query.message.answer(f"Вы выбрали качество: {chosen_quality}")
+    video_url = (await video[chosen_quality_index].last_url).mp4
+    seconds, width_clip, height_clip = await get_video_params(video_url)
+    await send_video(video_url, seconds, width_clip, height_clip, query.message.chat.id)
+
+
 @dp.message_handler()
-async def markups():
+async def main_markups():
     markup = types.InlineKeyboardMarkup()
     markup.row(
         types.InlineKeyboardButton('Назад', callback_data='back'),
@@ -93,12 +130,26 @@ async def markups():
     return markup
 
 
+@dp.message_handler()
+async def choose_translator_markups():
+    markup = types.InlineKeyboardMarkup()
+
+    for name, id_, in player.post.translators.name_id.items():
+        markup.add(types.InlineKeyboardButton(name, callback_data=name))
+    return markup
+
+
+@dp.message_handler()
+async def choose_quality_markups():
+    markup = types.InlineKeyboardMarkup()
+    for i in range(len(video.qualities)):
+        markup.add(types.InlineKeyboardButton(video.qualities[i], callback_data=i))
+    return markup
+
+
 async def next_film(chat_id):
+    global film
     page = 1
-    await bot.send_message(chat_id, 'Следующий фильм')
-    search_results = await search_results_queue.get()
-    film = await film_queue.get()
-    markup = await markup_queue.get()
     results = len(search_results)
 
     if film < results - 1:
@@ -113,20 +164,14 @@ async def next_film(chat_id):
     print(f'Тип контента - {content}')
 
     await bot.send_photo(chat_id, search_results[film].poster, player.post.name,
-                         reply_markup=markup)
-
-    await film_queue.put(film)
-    await markup_queue.put(markup)
-    await search_results_queue.put(search_results)
+                         reply_markup=markup_main)
 
     await asyncio.sleep(2)
 
+
 async def back_film(chat_id):
+    global film
     page = 1
-    await bot.send_message(chat_id, 'Следующий фильм')
-    search_results = await search_results_queue.get()
-    film = await film_queue.get()
-    markup = await markup_queue.get()
 
     if film > 0:
         film -= 1
@@ -140,13 +185,34 @@ async def back_film(chat_id):
     print(f'Тип контента - {content}')
 
     await bot.send_photo(chat_id, search_results[film].poster, player.post.name,
-                         reply_markup=markup)
-
-    await film_queue.put(film)
-    await markup_queue.put(markup)
-    await search_results_queue.put(search_results)
+                         reply_markup=markup_main)
 
     await asyncio.sleep(2)
+
+
+async def process_film():
+    global video, translator_id, player
+
+    meta_tag = player.post._soup_inst.find('meta', property='og:type')
+    content = meta_tag['content'].removeprefix('video.')
+
+    logging.info(f'Название - {player.post.name}')
+    logging.info(f'Переводчик - {player.post.translators.names}')
+    logging.info(f'Тип контента - {content}')
+    logging.info(f'URL фильма - {player.post.url}')
+
+    await asyncio.sleep(1.6)
+
+    if content == 'movie':
+        print('видео')
+        if player.post.translators.names in player.post.translators.name_id.items() is not None:
+            stream = await player.get_stream(translator_id)
+            video = stream.video
+        else:
+            stream = await player.get_stream()
+            video = stream.video
+    else:
+        print('это сериал пока не работаем с сериалами')
 
 
 async def get_video_params(video_file):
@@ -172,11 +238,11 @@ async def send_video(video_url, seconds, width_clip, height_clip, chat_id):
                             await f.write(chunk)
                             pbar.update(len(chunk))
                     pbar.close()
-                    # await telethon_client.send_file(
-                    #     chat_id, video_url.split('/')[-1],
-                    #     supports_streaming=True,
-                    #     attributes=[DocumentAttributeVideo(seconds, width_clip, height_clip, supports_streaming=True)]
-                    # )
+                    await telethon_client.send_file(
+                        chat_id, video_url.split('/')[-1],
+                        supports_streaming=True,
+                        attributes=[DocumentAttributeVideo(seconds, width_clip, height_clip, supports_streaming=True)]
+                    )
                     logging.info("Видео отправлено!")
 
                     os.remove(video_url.split('/')[-1])
