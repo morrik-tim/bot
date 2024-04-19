5fa161776b1b4316d890fae8fed8a461381ad19d
morrik <marattim83@gmail.com>
2024-04-19 23:40:44 +0500

    серьезные изменения

--- optimus.py
+++ optimus.py
@@ -2,9 +2,10 @@ import asyncio
 import logging
 import os
 import random
-
+import datetime
 import aiofiles
 import aiohttp
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
@@ -32,10 +33,10 @@ dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
 
-# Текстовые хэндлеры
-@dp.message_handler(commands=['info'])
-async def start(message: types.Message):
-    await message.answer(f'ID чата {message.chat.id}')
+@dp.message_handler(content_types=['video'])
+async def reply_video(message: types.Message):
+    video_ = message.video.file_id
+    await bot.send_video(chat_id=reply_id, video=video_)
 
 
 @dp.message_handler(commands=['start'])
@@ -48,16 +49,13 @@ async def start(message: types.Message):
 
 @dp.message_handler(content_types=['text'])
 async def main(message: types.Message):
-    global player, search_results, film, markup_main, choose_markup, page, reply_id
+    global player, search_results, film, markup_main, page, reply_id, del_msg_id
 
     reply_id = message.chat.id
-
     film = 0
     page = 1
-    markup_main = await main_markups()
 
     search_results = await Search(message.text).get_page(page)
-
     player = await search_results[film].player
 
     meta_tag = player.post._soup_inst.find('meta', property='og:type')
@@ -66,18 +64,14 @@ async def main(message: types.Message):
     print(f'Название - {player.post.name}')
     print(f'Тип контента - {content}')
 
-    film = 0
-    await bot.delete_message(message.chat.id, message.message_id - 1)
+    del_msg_id = message.message_id
+
+    markup_main = await main_markups()
+    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id - 1)
+    await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id - 2)
     await message.answer_photo(search_results[film].poster, player.post.name, reply_markup=markup_main)
 
 
-@dp.message_handler(content_types=['video'])
-async def reply_video(message: types.Message):
-    video_ = message.video.file_id
-    await bot.send_video(chat_id=reply_id, video=video_)
-
-
-# Хэндлеры кнопок
 @dp.callback_query_handler(lambda query: query.data == 'select')
 async def select_callback_handler(query: types.CallbackQuery):
     choose_markup = await choose_translator_markups()
@@ -92,6 +86,13 @@ async def select_callback_handler(query: types.CallbackQuery):
     )
 
 
+@dp.callback_query_handler(lambda query: query.data == 'new_search')
+async def new_search_callback_handler(query: types.CallbackQuery):
+    await bot.delete_message(chat_id=query.message.chat.id, message_id=del_msg_id)
+    await query.message.delete()
+    await query.message.answer("Введите название фильма или сериала.")
+
+
 @dp.callback_query_handler(lambda query: query.data == 'next')
 async def next_callback_handler(query: types.CallbackQuery):
     await next_film(query.message.chat.id, query.message.message_id)
@@ -100,6 +101,11 @@ async def next_callback_handler(query: types.CallbackQuery):
 @dp.callback_query_handler(lambda query: query.data == 'back')
 async def back_callback_handler(query: types.CallbackQuery):
     await back_film(query.message.chat.id, query.message.message_id)
+
+
+@dp.callback_query_handler(lambda query: query.data == 'back2menu')
+async def back_callback_handler(query: types.CallbackQuery):
+    await back2menu(query.message.chat.id, query.message.message_id)
 
 
 @dp.callback_query_handler(lambda query: query.data in player.post.translators.name_id.keys())
@@ -113,9 +119,34 @@ async def translator_callback_handler(query: types.CallbackQuery):
     await asyncio.sleep(5)
     await process_film()
 
+    choose_quality = await choose_quality_markups()
+
     if video is not None:
-        choose_quality = await choose_quality_markups()
-        # await query.message.edit_text('Выберете качество', reply_markup=choose_quality)
+        await bot.edit_message_media(
+            chat_id=query.message.chat.id,
+            message_id=query.message.message_id,
+            media=types.InputMediaPhoto(
+                media=search_results[film].poster,
+                caption=f'Выберете качество для {player.post.name}'),
+            reply_markup=choose_quality
+        )
+    else:
+        print("ОШИБКА!!!")
+
+
+@dp.callback_query_handler(lambda query: query.data == 'original')
+async def translator_callback_handler(query: types.CallbackQuery):
+    global translator_id, choose_quality, video, player
+
+    translator_id = None
+
+    # Далее вы можете выполнить какие-то действия в зависимости от выбранного переводчика
+    await asyncio.sleep(5)
+    await process_film()
+
+    choose_quality = await choose_quality_markups()
+
+    if video is not None:
         await bot.edit_message_media(
             chat_id=query.message.chat.id,
             message_id=query.message.message_id,
@@ -155,9 +186,11 @@ async def main_markups():
     markup = types.InlineKeyboardMarkup()
 
     markup.row(
-        types.InlineKeyboardButton('Назад', callback_data='back'),
-        types.InlineKeyboardButton('Далее', callback_data='next'))
-    markup.add(types.InlineKeyboardButton('Выбрать', callback_data='select'))
+        types.InlineKeyboardButton(text='Назад', callback_data='back'),
+        types.InlineKeyboardButton(text='Далее', callback_data='next'))
+
+    markup.row(types.InlineKeyboardButton(text='Выбрать', callback_data='select'))
+    markup.row(types.InlineKeyboardButton(text='Новый поиск', callback_data='new_search'))
 
     return markup
 
@@ -167,7 +200,12 @@ async def choose_translator_markups():
     markup = types.InlineKeyboardMarkup()
 
     for name, id_, in player.post.translators.name_id.items():
-        markup.add(types.InlineKeyboardButton(name, callback_data=name))
+        if name is None:
+            markup.add(types.InlineKeyboardButton('Оригинал', callback_data='original'))
+        else:
+            markup.add(types.InlineKeyboardButton(name, callback_data=name))
+
+    markup.add(types.InlineKeyboardButton('Назад', callback_data='back2menu'))
     return markup
 
 
@@ -176,13 +214,29 @@ async def choose_quality_markups():
     global video
 
     markup = types.InlineKeyboardMarkup()
-
-    for i in range(len(video.qualities)):
-        markup.add(types.InlineKeyboardButton(video.qualities[i], callback_data=i))
+    if video is not None:
+        for i in range(len(video.qualities)):
+            markup.add(types.InlineKeyboardButton(video.qualities[i], callback_data=i))
+        markup.add(types.InlineKeyboardButton('Назад', callback_data='back2menu'))
+    else:
+        print("О��ИБКА!!!")
     return markup
 
 
 # Методы
+async def back2menu(chat_id, message_id):
+    global film, player
+    player = await search_results[film].player
+
+    await bot.edit_message_media(
+        chat_id=chat_id,
+        message_id=message_id,
+        media=types.InputMediaPhoto(
+            media=search_results[film].poster,
+            caption=player.post.name),
+        reply_markup=markup_main)
+
+
 async def next_film(chat_id, message_id):
     global film, player
     results = len(search_results)
@@ -240,6 +294,7 @@ async def process_film():
         stream = await player.get_stream(translator_id)
         video = stream.video
     else:
+        video = None
         print('Это сериал, пока не работаем с сериалами')
 
 
@@ -256,12 +311,15 @@ async def upload_progress_callback(current, total):
     current_mb = current / (1024 * 1024)
     total_mb = total / (1024 * 1024)
 
-    print(f"Uploaded {current_mb:.2f} MB out of {total_mb:.2f} MB")
+    # Получаем текущее время
+    now = datetime.datetime.now()
+    # Форматируем время в формат чч:мм
+    formatted_time = now.strftime("%H:%M")
+
+    print(f"Uploaded {current_mb:.2f} MB out of {total_mb:.2f} MB at {formatted_time}")
 
 
 async def send_video(video_url_, seconds_, width_clip_, height_clip_, chat_id):
-    global gl
-    gl = chat_id
     const_chat_id = -1002112068525
     timeout = aiohttp.ClientTimeout(total=3600)  # Установите подходящее значение таймаута
     async with aiohttp.ClientSession(timeout=timeout) as session:
@@ -285,7 +343,7 @@ async def send_video(video_url_, seconds_, width_clip_, height_clip_, chat_id):
                         caption=player.post.name,
                         supports_streaming=True,
                         use_cache=True,
-                        part_size_kb=2048,
+                        part_size_kb=8192,
                         attributes=[DocumentAttributeVideo(seconds_, width_clip_, height_clip_)],
                         progress_callback=upload_progress_callback,
                         file_size=content_length
